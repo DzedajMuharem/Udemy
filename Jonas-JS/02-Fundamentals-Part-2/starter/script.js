f8a29b1474a1bfe17adc1357eff5f5c3253c5098
DzedajMuharem <mujanovich100@gmail.com>
2023-01-04 19:07:40 +0100

    36.

---
+++ Jonas-JS/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+/*
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+*/
+
+/*
+function logger() {
+  console.log("My name is Hasan");
+}
+// calling / running / invoking function (pozivanje funcikije)
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+
+*/
+
+/*
+
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
+
+*/
+
+/*
+
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement}`;
+};
+console.log(yearsUntilRetirement(1991, "Hasan"));
+console.log(yearsUntilRetirement(2005, "Bob"));
+
+*/
+
+/*
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} piece of oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
