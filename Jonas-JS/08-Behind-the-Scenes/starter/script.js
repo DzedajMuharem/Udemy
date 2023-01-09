e8add88890fcfb731efbf081c10f71b2723b63df
DzedajMuharem <mujanovich100@gmail.com>
2023-01-09 03:43:08 +0100

    98.

--- Jonas-JS/08-Behind-the-Scenes/starter/script.js
+++ Jonas-JS/08-Behind-the-Scenes/starter/script.js
@@ -41,6 +41,8 @@ console.log(calcAge(1983));
 
 */
 
+/*
+
 // Variables
 console.log(me);
 // console.log(job);
@@ -81,3 +83,70 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.x);
 console.log(z === window.x);
+
+*/
+
+/*
+
+console.log(this);
+const calcAge = function (birthYear) {
+  console.log(2034 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2034 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1990);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+// Methode borrowing
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+
+console.log(f);
+
+f();
+
+*/
+// var firstName = 'Matilda';
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    const self = this; // self or that
+    const isMillenial = function () {
+      console.log(self);
+      console.log(self.year >= 1981 && self.year <= 1996);
+      //   console.log(this);
+      //   console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: function () {
+    console.log(this);
+
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
