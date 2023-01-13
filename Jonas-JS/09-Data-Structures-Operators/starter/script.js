91c59b132b8355546c7e4909ea864d356f14f6f5
DzedajMuharem <mujanovich100@gmail.com>
2023-01-13 03:26:52 +0100

    coding challenge 1

--- Jonas-JS/09-Data-Structures-Operators/starter/script.js
+++ Jonas-JS/09-Data-Structures-Operators/starter/script.js
@@ -50,6 +50,132 @@ const restaurant = {
     console.log(mianIngridient, otherIngridients);
   },
 };
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+const team1 = game.odds.team1;
+const draw = game.odds.x;
+const team2 = game.odds.team2;
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+let team1W = team1 > team2 && 'Team 1 ima vise sansi za pobedu';
+let team2W = team1 < team2 && 'Team 2 ima vise sansi za pobedu';
+let message = team1W || team2W;
+console.log(message);
+
+const substitute = ['Thiago', 'Coutinho', 'Perisic'];
+const players1Final = [...game.players[0], ...substitute];
+console.log(players1Final);
+const allPlayers = [...game.players[0], ...game.players[1]];
+// console.log(allPlayers);
+const [gk, ...fieldPlayers] = game.players[[0]];
+console.log(gk, fieldPlayers);
+function neka(...broj) {
+  console.log(`${broj}, goals scored ${game.scored.length}`);
+}
+neka(game.scored);
+/*
+const rest1 = {
+  name: 'Capri',
+  // numGuest: 20,
+  numGuest: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignmet=nt operator //
+// rest1.numGuest = rest1.numGuest || 10;
+// rest2.numGuest = rest2.numGuest || 10;
+
+// rest1.numGuest ||= 10;
+// rest2.numGuest ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuest ??= 10;
+rest2.numGuest ??= 10;
+
+// AND assignmet=nt operator //
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+// IF YOU EVER NEDD TO ASSIGN A VALUE TO A VARIABLE THAT IS ALREDY DEFINED, THAT HAS A VALUES THAT IS CURRENTLY TRUTHY, THEN YOU CAN USE AND ASSIGNMENT OPERATOR
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1, rest2);
+*/
+
+/*
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+// Nullish values: null and undefined (NOT 0 or '')
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
+*/
 
 /*
 
