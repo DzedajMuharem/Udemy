67a28f28debc4340bdd2ebf93d122462bca860e0
DzedajMuharem <mujanovich100@gmail.com>
2023-01-21 04:02:09 +0100

    Section 10

--- Jonas-JS/10-Functions/starter/script.js
+++ Jonas-JS/10-Functions/starter/script.js
@@ -238,6 +238,7 @@ GOOD LUCK 😀
 //         3: C++
 //         (Write option number)
 
+/*
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
@@ -291,3 +292,110 @@ console.log(test1([2, 4, 3]));
 
 poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 poll.displayResults.call({ answers: [1, 4, 6, 3, 7, 2] });
+*/
+
+/*
+// Immediately Invoked Function Expressions (IIFE)
+
+const runOnce = function () {
+  console.log('This weiil never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+// console.log(isPrivate);
+
+(() => console.log('this weill aslo never run again'))();
+
+// we can use this instead {} just to vreate a block for let and const
+{
+  const isPrivate = 23;
+  var notPrivate = 54;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+*/
+
+/*
+// Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+console.dir(booker);
+*/
+
+/*
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGrp = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups each with ${perGrp} passengers`);
+  }, (wait = 1000));
+  console.log(`We will start boarind in ${wait} seconds`);
+};
+
+// Closure have priority
+const perGrp = 1000;
+boardPassengers(180, 3);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  const change = document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
