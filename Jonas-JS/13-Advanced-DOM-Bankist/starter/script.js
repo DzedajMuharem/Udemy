d0e49eddb93f2d7c6ae4d7c9ae2c52c918ce9633
DzedajMuharem <mujanovich100@gmail.com>
2023-01-29 01:25:08 +0100

    186.

--- Jonas-JS/13-Advanced-DOM-Bankist/starter/script.js
+++ Jonas-JS/13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,48 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////////////////
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSection = document.querySelectorAll('.section');
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn---close-cookie">Got it!</button>`;
+
+// Ads element as first child on an element
+// header.prepend(message);
+// Ads element as last child on an element
+header.append(message);
+// There can only be one element at the time. We just moved element here
+
+// header.prepend(message.cloneNode(true));
+// Here it will show at the start and at the bottom cuz we cloned it
+
+// header.before(message);
+header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn---close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+
+    message.parentElement.removeChild(message);
+  });
