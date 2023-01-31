5c48b037795b60479edbdb0bcd98e332ac19f230
DzedajMuharem <mujanovich100@gmail.com>
2023-01-31 14:02:51 +0100

    188.

--- Jonas-JS/13-Advanced-DOM-Bankist/starter/script.js
+++ Jonas-JS/13-Advanced-DOM-Bankist/starter/script.js
@@ -74,3 +74,85 @@ document
 
     message.parentElement.removeChild(message);
   });
+
+/*
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// For custom properties
+// Set property can be used for all properties too, al sto
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo'; // -we chanced attribute
+
+// Non-standard
+// console.log(logo.designer); - we get undefined
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist'); // - makin new attribute
+
+console.log(logo.src); // - absolute version
+console.log(logo.getAttribute('src')); // - relative version
+
+const link = document.querySelector('.nav__link--btn');
+
+console.log(link.href); // - absolute
+console.log(link.getAttribute('href')); // - relative
+
+// Data attributes // dataset
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'b');
+logo.classList.toggle('c', 'as');
+logo.classList.contains('c', 'asd'); // not includes
+
+// Dont use// overrides all existing classes and allows only one class
+logo.className = 'jonas';
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+  console.log(
+    'heigh/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
