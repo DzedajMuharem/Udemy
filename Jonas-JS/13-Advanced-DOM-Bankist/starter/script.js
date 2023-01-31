e30575fdedbc7e54e7db12f84984dc67110a2237
DzedajMuharem <mujanovich100@gmail.com>
2023-01-31 19:21:39 +0100

    195.

--- Jonas-JS/13-Advanced-DOM-Bankist/starter/script.js
+++ Jonas-JS/13-Advanced-DOM-Bankist/starter/script.js
@@ -11,6 +11,12 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
+
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -32,6 +38,8 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Event Delegation: Implementing Page Navigation
 
 // Button scroling
 btnScrollTo.addEventListener('click', function (e) {
@@ -88,6 +96,79 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+/////////////////////////////////////////////////////////
+// Building a Tabbed Component
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+///////////////////////////////////////////////////////
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// nav.addEventListener('mouseover', function (e) {
+//   // if (e.target.classList.contains('nav__link')) {
+//   //   const link = e.target;
+//   //   const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//   //   const logo = link.closest('.nav').querySelector('img');
+
+//   //   siblings.forEach(el => {
+//   //     if (el !== link) el.style.opacity = 0.5;
+//   //   });
+//   //   logo.style.opacity = 0.5;
+//   // }
+// });
+
+// Passing and "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1);
+// });
+
+// nav.addEventListener('mouseout', function (e) {
+//   if (e.target.classList.contains('nav__link')) {
+//     const link = e.target;
+//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//     const logo = link.closest('.nav').querySelector('img');
+
+//     siblings.forEach(el => {
+//       if (el !== link) el.style.opacity = 1;
+//     });
+//     logo.style.opacity = 1;
+//   }
+// });
 
 //////////////////////////////////////////////////////////////
 
@@ -271,4 +352,36 @@ document.querySelector('.nav').addEventListener(
 // Addin the thirs paramater our variable is waiting for capture while others wait for bubble
 */
 
-// Event Delegation: Implementing Page Navigation
+// DOM Traversing
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+
+// Only for direct childs
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'red';
+
+// Going uppwards
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = `var(--gradient-secondary)`;
+
+// h1.closest('h1').style.background = `var(--gradient-primary)`;
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
