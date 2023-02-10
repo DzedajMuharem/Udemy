279f5b1e3c3eafc3e8a667aa113a9990a31777ae
DzedajMuharem <mujanovich100@gmail.com>
2023-02-10 23:52:27 +0100

    208.

--- Jonas-JS/14-OOP/starter/script.js
+++ Jonas-JS/14-OOP/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+//Constructor Functions and the new Operator
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Neve do this
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {}(new empty object) is created
+// 2. Function is called , this keyword = {}
+// 3.{} linked to prototype
+// 4. function automatically return{}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
