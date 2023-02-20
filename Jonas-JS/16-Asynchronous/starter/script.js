d0cbb24e5f6cc6d94bde08380a626ecbe4e579b6
DzedajMuharem <mujanovich100@gmail.com>
2023-02-20 23:59:35 +0100

    253.

--- Jonas-JS/16-Asynchronous/starter/script.js
+++ Jonas-JS/16-Asynchronous/starter/script.js
@@ -117,3 +117,33 @@ setTimeout(() => {
 
 const request = fetch('https://restcountries.com/v2/name/serbia');
 console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+
+getCountryData('serbia');
+// getCountryData('germany');
