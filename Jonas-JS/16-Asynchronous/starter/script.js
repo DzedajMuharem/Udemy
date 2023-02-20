f683183ab6b732909e51f5f372ef719b89c04077
DzedajMuharem <mujanovich100@gmail.com>
2023-02-20 23:40:28 +0100

    251.

--- Jonas-JS/16-Asynchronous/starter/script.js
+++ Jonas-JS/16-Asynchronous/starter/script.js
@@ -7,18 +7,43 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-  // console.log(this.responseText);
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+//   // console.log(this.responseText);
 
-  request.addEventListener('load', function () {
-    console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText); //This is array containing one object so we can destructure it
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     console.log(this.responseText);
+//     const [data] = JSON.parse(this.responseText); //This is array containing one object so we can destructure it
+//     console.log(data);
 
-    const html = ` <article class="country">
+//     const html = ` <article class="country">
+//   <img class="country__img" src="${data.flag}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       +data.population / 1000000
+//     ).toFixed(1)} people</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+// </article>`;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('serbia');
+// getCountryData('usa');
+// getCountryData('germany');
+
+// Welcome to Callback Hell
+
+const renderCountry = function (data, className = '') {
+  const html = ` <article class="country ${className}">
   <img class="country__img" src="${data.flag}" />
   <div class="country__data">
     <h3 class="country__name">${data.name}</h3>
@@ -31,11 +56,64 @@ const getCountryData = function (country) {
   </div>
 </article>`;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+/*
+const getCountryAndNeighbour = function (country) {
+  // Ajax call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  // console.log(this.responseText);
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText); //This is array containing one object so we can destructure it
+    console.log(data);
+
+    //Render country 1
+    renderCountry(data);
+
+    // Get neighbour country
+    const neighbour = data.borders?.[0];
+
+    // Ajax call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('serbia');
-getCountryData('usa');
-getCountryData('germany');
+// getCountryAndNeighbour('serbia');
+getCountryAndNeighbour('turkey');
+
+setTimeout(() => {
+  console.log(`1 sec passed`);
+  setTimeout(() => {
+    console.log(`2 sec passed`);
+    setTimeout(() => {
+      console.log(`3 sec passed`);
+      setTimeout(() => {
+        console.log(`4 sec passed`);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+*/
+
+// Promises and the Fetch API
+
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+const request = fetch('https://restcountries.com/v2/name/serbia');
+console.log(request);
