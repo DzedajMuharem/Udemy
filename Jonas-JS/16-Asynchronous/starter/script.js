52c2635d8fb136c4c75cb26c25e1ed5d546b3517
DzedajMuharem <mujanovich100@gmail.com>
2023-02-24 23:01:00 +0100

    Coding Challenge #2

--- Jonas-JS/16-Asynchronous/starter/script.js
+++ Jonas-JS/16-Asynchronous/starter/script.js
@@ -400,10 +400,16 @@ PART 1
 1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
 */
 let divLocation = document.querySelector('.images');
-let imgElement = document.createElement('img');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
 const createImage = function (imgPath) {
   return new Promise(function (resolve, reject) {
+    let imgElement = document.createElement('img');
     imgElement.src = imgPath;
 
     imgElement.addEventListener('load', function () {
@@ -416,7 +422,24 @@ const createImage = function (imgPath) {
     });
   });
 };
+let currentImg;
 
-createImage('https://media.geeksforgeeks.org/gfg-gg-logo.svg').catch(err =>
-  console.error(err)
-);
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log(currentImg);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log(currentImg);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
