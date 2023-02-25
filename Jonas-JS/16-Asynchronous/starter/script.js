bfce43c9e9319690aa44a276e60a317b693dd83b
DzedajMuharem <mujanovich100@gmail.com>
2023-02-26 00:24:46 +0100

    16-Asynchronous

--- Jonas-JS/16-Asynchronous/starter/script.js
+++ Jonas-JS/16-Asynchronous/starter/script.js
@@ -539,6 +539,7 @@ const get3Countries = async function (c1, c2, c3) {
 get3Countries('portugal', 'canada', 'serbia');
 */
 
+/*
 // Other Promise Combinators: race, allSettled and any
 
 // Promise.race - we get only one result. Whoever loads first. Promise that gets rejected also can be accepted
@@ -591,3 +592,86 @@ Promise.any([
 ])
   .then(res => console.log(res))
   .catch(err => console.error(err));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+let divLocation = document.querySelector('.images');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = async function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    let imgElement = document.createElement('img');
+    imgElement.src = imgPath;
+
+    imgElement.addEventListener('load', function () {
+      divLocation.append(imgElement);
+      resolve(imgElement);
+    });
+
+    imgElement.addEventListener('error', function () {
+      reject(new Error('No img'));
+    });
+  });
+};
+
+let currentImg;
+const loadNPause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    console.log(`img 1 loaded`);
+    await wait(2);
+    img.style.display = 'none';
+    let img2 = await createImage('img/img-2.jpg');
+    console.log(`img 2 loaded`);
+    await wait(2);
+    img2.style.display = 'none';
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// loadNPause();
+
+// PART 2
+// 1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+// 2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+// 3. Check out the 'imgs' array in the console! Is it like you expected?
+// 4. Use a promise combinator function to actually get the images from the array 😉
+// 5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+// TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+const loadAll = async function (imgArr) {
+  const imgs = imgArr.map(async img => await createImage(img));
+  console.log(imgs);
+
+  const imgsEl = await Promise.all(imgs);
+  console.log(imgsEl);
+
+  imgsEl.forEach(img => img.classList.add('parallel'));
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
