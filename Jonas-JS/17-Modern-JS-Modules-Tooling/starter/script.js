e9c04ae4235c4036de847e270b6e2102f2e4f24e
DzedajMuharem <mujanovich100@gmail.com>
2023-03-13 18:50:46 +0100

    275.

--- Jonas-JS/17-Modern-JS-Modules-Tooling/starter/script.js
+++ Jonas-JS/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -28,6 +28,7 @@ console.log(cart);
 
 // console.log('Something');
 
+/*
 const getLastPost = async function () {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
@@ -42,3 +43,46 @@ console.log(lastPost);
 // lastPost.then(last => console.log(last));
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+/*
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('apple', 4);
+console.log(shoppingCart2);
+console.log(shoppingCart2.shippingCost);
+*/
+
+/*
+// Export
+export.addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  // Import 
+const{addToCart} = require('./shoppingCart.js')
+*/
