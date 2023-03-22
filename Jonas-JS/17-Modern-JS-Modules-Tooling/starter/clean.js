8d32bfb239513a2a62355a1adeeb7117dbca1cfc
DzedajMuharem <mujanovich100@gmail.com>
2023-03-23 00:29:01 +0100

    281.

--- Jonas-JS/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ Jonas-JS/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -31,9 +31,9 @@ const addExpense = function (value, description, user = 'jonas') {
 
   // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
 
-  const limit = getLimit(user);
+  // const limit = getLimit(user);
 
-  if (value <= limit) {
+  if (value <= getLimit(user)) {
     // budget.push({ value: -value, description: description, user: user });
 
     budget.push({ value: -value, description, user });
@@ -64,15 +64,18 @@ checkExpenses();
 
 console.log(budget);
 
-const bigExpenses = function (limit) {
+const logBigExpenses = function (bigLimit) {
   let output = '';
-  for (const el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
+  for (const entry of budget) {
+    // if (entry.value <= -bigLimit) {
+    //   output += `${entry.description.slice(-2)} /`; // Emojis are 2 chars
+    // }
+
+    output +=
+      entry.value <= -bigLimit ? `${entry.description.slice(-2)} /` : '';
   }
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
 
-bigExpenses(1000);
+logBigExpenses(1000);
